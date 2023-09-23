697dced378629bc3c1c609abd9d72b269be2e59b
Dan Friedman <dan@thefriedmans.org>
2023-09-23 08:58:45 -0500

    Get required units

---
+++ src/Wayfinder.Services/RequiredUnits/RequiredUnitsModels.cs
@@ -0,0 +1,5 @@
+﻿namespace Wayfinder.Services.RequiredUnits;
+
+public record GoalUnit(string UnitId, List<Requirement> Requirements);
+
+public record Requirement(string UnitId, string Level);
---
+++ tests/Wayfinder.Tests/Services/GetRequiredUnitsTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Wayfinder.Services.RequiredUnits;
+
+namespace Wayfinder.Tests.Services;
+
+public class GetRequiredUnitsTests
+{
+    [Fact]
+    public async Task Has_multiple_units()
+    {
+        var units = await RequiredUnitsService.GetAllRequirementsAsync();
+        units.Should().HaveCountGreaterThan(10);
+    }
+
+    [Theory]
+    [InlineData("LV")]
+    [InlineData("EXECUTOR")]
+    public async Task Has_unit(string unitId)
+    {
+        var units = await RequiredUnitsService.GetAllRequirementsAsync();
+        units.Should().Contain(x => x.UnitId.Equals(unitId, StringComparison.InvariantCultureIgnoreCase));
+    }
+
+    [Theory]
+    [InlineData("LV", 15)]
+    public async Task Has_X_requirements(string unitId, int numOfRequirements)
+    {
+        var units = await RequiredUnitsService.GetAllRequirementsAsync();
+        var unit = units.First(x => x.UnitId.Equals(unitId, StringComparison.InvariantCultureIgnoreCase));
+        unit.Requirements.Should().HaveCount(numOfRequirements);
+    }
+}
