7bddd68bc430bcb031d229072f9495813b3be16d
Dan Friedman <dan@thefriedmans.org>
2023-09-25 12:27:15 -0500

    Add requirement view model

---
+++ src/Wayfinder.Services/RequiredUnits/RequiredUnitsViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Wayfinder.Services.Models;
+
+namespace Wayfinder.Services.RequiredUnits;
+
+public partial class RequiredUnitsViewModel : ObservableObject
+{
+    [ObservableProperty] List<Unit> _selectedUnits = default!;
+    [ObservableProperty] private List<RequiredUnit> _units = default!;
+
+    public IReadOnlyList<Unit> AllUnits { get; set; } = default!;
+    public List<GoalUnit> AllObjectives { get; set; } = default!;
+
+
+    public async Task InitializeAsync()
+    {
+        await LoadUnitsAsync();
+        await LoadRequirementsAsync();
+
+        var query = from r in AllObjectives
+                    join u in AllUnits on r.UnitId equals u.BaseId
+                    orderby u.Name
+                    select u;
+
+        SelectedUnits = query.ToList();
+    }
+
+    private async Task LoadUnitsAsync() => AllUnits = await GameService.GetUnitsAsync();
+
+    private async Task LoadRequirementsAsync() => AllObjectives = await RequiredUnitsService.GetAllRequirementsAsync();
+
+    public void LoadRequiredUnits()
+    {
+        var selectedUnitIds = SelectedUnits.Select(x => x.BaseId).ToArray();
+        var objectives = AllObjectives.Where(x => selectedUnitIds.Contains(x.UnitId));
+        var requirements = objectives.SelectMany(x => x.Requirements).ToList();
+
+        var unitDict = AllUnits.ToDictionary(x => x.BaseId, x => x.Name);
+
+        var query = from g in objectives
+                    from r in g.Requirements
+                    select new { GoalUnit = g.UnitId, RequiredUnit = r.UnitId, RequiredLevel = r.Level } into d
+                    group d by d.RequiredUnit into ru
+                    select new RequiredUnit(ru.Key, ru.Select(x => new RequiredDetail(x.GoalUnit, x.RequiredLevel)).ToList())                    ;
+
+        Units = query.ToList();
+    }
+}
+
+public record RequiredDetail(string GoalUnitId, string Level);
+
+public record RequiredUnit(string UnitId, List<RequiredDetail> Details);
---
+++ tests/Wayfinder.Tests/RequiredUnits/ViewModel.LoadUnitsTests.cs
@@ -0,0 +1,60 @@
+﻿using FluentAssertions;
+using Wayfinder.Services.RequiredUnits;
+
+namespace Wayfinder.Tests.RequiredUnits
+{
+    public class LoadUnitsTests
+    {
+        private readonly RequiredUnitsViewModel _viewModel = new();
+
+        [Fact]
+        public async Task List_all_units_for_one_character()
+        {
+            await _viewModel.InitializeAsync();
+
+            var selectedUnits = _viewModel.AllUnits.Where(x => x.BaseId == "LORDVADER");
+            _viewModel.SelectedUnits = selectedUnits.ToList();
+
+            _viewModel.LoadRequiredUnits();
+
+            _viewModel.Units.Should().HaveCount(15);
+        }
+
+        [Fact]
+        public async Task All_goal_units_are_selected_by_default()
+        {
+            await _viewModel.InitializeAsync();
+
+            _viewModel.SelectedUnits.Should().HaveSameCount(_viewModel.AllObjectives);
+        }
+
+        [Fact]
+        public async Task All_goal_units_have_at_least_five_requirements()
+        {
+            await _viewModel.InitializeAsync();
+
+            var selectedUnitIds = new[] { "JABBATHEHUTT", "GRANDMASTERLUKE" };
+            var selectedUnits = _viewModel.AllUnits.Where(x => selectedUnitIds.Contains(x.BaseId));
+            _viewModel.SelectedUnits = selectedUnits.ToList();
+
+            _viewModel.LoadRequiredUnits();
+
+            _viewModel.Units.Should().HaveCountGreaterThan(15).And.HaveCountLessThan(30);
+        }
+
+        [Fact]
+        public async Task Both_have_requirements_for_CLS()
+        {
+            await _viewModel.InitializeAsync();
+
+            var selectedUnitIds = new[] { "JABBATHEHUTT", "GRANDMASTERLUKE" };
+            var selectedUnits = _viewModel.AllUnits.Where(x => selectedUnitIds.Contains(x.BaseId));
+            _viewModel.SelectedUnits = selectedUnits.ToList();
+
+            _viewModel.LoadRequiredUnits();
+
+            var unit = _viewModel.Units.Should().Contain(x => x.UnitId == "JEDIKNIGHTLUKE").Subject;
+            unit.Details.Should().HaveCount(2);
+        }
+    }
+}
